46e98de51d1a19f626327d0a58221ca8c2b256ba
Beatriz Nonato <bea_nonato@hotmail.com>
2020-10-05 10:26:23 -0300

    Alteração Materiais - Página Inicio

--- cosmeticos/index.php
+++ cosmeticos/index.php
@@ -15,22 +15,22 @@ Lenne cosmetics</title>
     <div class="col-sm-4">
       <div class="panel panel-primary">
         <div class="panel-heading">Ofertas</div>
-        <div class="panel-body"><img src="imagens/S.O.S (21).png" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer">Eleve <p class="text-danger">Preço: <s>15,99 R$</s>12,99 R$</p></div>
+        <div class="panel-body"><img src="Português.png" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">Resumo de Português <p class="text-danger">Preço: <s>15,99 R$</s>12,99 R$</p></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-danger">
         <div class="panel-heading">Ofertas</div>
-        <div class="panel-body"><img src="imagens/cicatricure.jpg" class="img-responsive" style="width:53%" alt="Image"></div>
-<div class="panel-footer">Cicatricure Gel  <p class="text-danger">Preço: <s>85,99 R$</s>79,99 R$</p></div>
+        <div class="panel-body"><img src="Matemática (5).png" class="img-responsive" style="width:53%" alt="Image"></div>
+<div class="panel-footer">Resumo de Matemática <p class="text-danger">Preço: <s>85,99 R$</s>79,99 R$</p></div>
       </div>
     </div>
     <div class="col-sm-4"> 
       <div class="panel panel-success">
         <div class="panel-heading">Ofertas</div>
-        <div class="panel-body"><img src="imagens/corpo-e-rosto.jpg" class="img-responsive" style="width:86%" alt="Image"></div>
-        <div class="panel-footer">Quit corpo & rosto  <p class="text-danger">Preço: <s>45,99 R$</s>39,99 R$</p></div>
+        <div class="panel-body"><img src="AOC (2).png" class="img-responsive" style="width:86%" alt="Image"></div>
+        <div class="panel-footer">Resumo de AOC  <p class="text-danger">Preço: <s>45,99 R$</s>39,99 R$</p></div>
       </div>
     </div>
   </div>
