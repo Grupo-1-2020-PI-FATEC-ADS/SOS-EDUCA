2dbbb548f81c5c1438cfe3d9df856bdbc1d89333
Nayara <nayara.lopes2007@gmail.com>
2020-10-05 11:41:42 -0300

    # Modificações da categorias

--- cosmeticos/index_carrinho_cliente.php
+++ cosmeticos/index_carrinho_cliente.php
@@ -10,12 +10,15 @@
     <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
      <meta charset="utf-8">
+     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>Carrinho</title>
     <style type="text/css" media="screen">
   
     </style>
+    
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <!-- As 3 meta tags acima *devem* vir em primeiro lugar dentro do `head`; qualquer outro conteúdo deve vir *após* essas tags -->
     
     <!-- Bootstrap -->
