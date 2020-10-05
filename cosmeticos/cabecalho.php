611cc15712e83e9b468843d881798e5bfc7e6104
Beatriz Nonato <bea_nonato@hotmail.com>
2020-10-05 10:05:51 -0300

    Altura logo

--- cosmeticos/cabecalho.php
+++ cosmeticos/cabecalho.php
@@ -39,7 +39,7 @@ Lenne cosmetics</title>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#"><img  src="imagens/S.O.S (22).png" alt="placeholder+image" height="40px" width="80px"></a>
+      <a class="navbar-brand" href="#"><img  src="imagens/S.O.S (22).png" alt="placeholder+image" height="80px" width="80px"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
