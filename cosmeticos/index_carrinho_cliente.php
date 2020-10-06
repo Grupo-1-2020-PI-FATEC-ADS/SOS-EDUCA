a6d5e3eef37c4dcaf72f8995cb448aa643916a52
Beatriz Nonato <bea_nonato@hotmail.com>
2020-10-06 19:40:54 -0300

    atualização linha 49 index carrinho cliente

--- cosmeticos/index_carrinho_cliente.php
+++ cosmeticos/index_carrinho_cliente.php
@@ -46,7 +46,7 @@
 
 </div><h1>
     <?php
-  $conexao=mysqli_connect("localhost", "root", "","sos_educa") or die(mysql_error()); ?>
+  $conexao=mysqli_connect("localhost", "root", "","cosmeticos") or die(mysql_error()); ?>
 
 <h3>Fitros de Pesquisa</h3>
     <form name="cons" method="post" action="index_carrinho_cliente.php">
