be924b6f2a003fc783d010abce14e9bfddcdd7e1
Nayara <nayara.lopes2007@gmail.com>
2020-10-09 16:07:34 -0300

    #Renomeando URL do PDF

--- SPRINT 1/_sos_educa/logout_carrinho.php
+++ SPRINT 1/_sos_educa/logout_carrinho.php
@@ -169,7 +169,7 @@ if (count($_SESSION['carrinho_cliente']) == 0) {
          <tr class="text-center">
             <td class="alert-info"> 
             
-               <a href="/Projeto_PI/SPRINT 1/_sos_educa/imagens/' . $nome . '"target="_blank">' . $nome . '</a>
+               <a href="/Projeto_PI/SOS_EDUCA/SPRINT 1/_sos_educa/imagens/' . $nome . '"target="_blank">' . $nome . '</a>
             </td>
             <td class="alert-warning"><input type="text" readonly="true" class="form-control"  name="prod[' . $id . ']" value="' . $qtd . '" /></td>
             <td class="alert-warning">R$ ' . $preco . '</td>
