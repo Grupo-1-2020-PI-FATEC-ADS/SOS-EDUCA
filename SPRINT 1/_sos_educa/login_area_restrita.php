4af70c7985a3b6fe58bb3f4eeff436e2d27999c4
Beatriz Nonato <bea_nonato@hotmail.com>
2020-10-09 10:31:27 -0300

    Indentação

--- SPRINT 1/_sos_educa/login_area_restrita.php
+++ SPRINT 1/_sos_educa/login_area_restrita.php
@@ -1,26 +1,26 @@
 
 <html>
-<head>
-	<title>Aréa Restrita</title>
+   <head>
+	   <title>Aréa Restrita</title>
 	
 	</head>
-<body>
-<?php include('cabecalho.php');?>
-<br><br>
+   <body>
+      <?php include('cabecalho.php');?>
+         <br><br>
 
-    <br>
+         <br>
     
-     <section class="newsletter container bg-black">
-           <h2 class="text-primary">ÁREA RESTRITA</h2>
-           <h3 class="text-danger">  Login </h3>
-           <form class="form-inline" action="autenticado_usuario.php" method="POST" >
-              <input class="form-control" type="email" name="email" placeholder="Login" value="<?php echo @$_SESSION['email']?>" ><br>
-                <input class="form-control" type="password" name="senha" placeholder="Sua senha" value="<?php echo @$_SESSION['senha']?>" ><br>
-              <button class="btn-success"> Entrar </button>
-           </form>
-        </section>
-	  <br>
-	  <?php include('rodape.php');?>
+            <section class="newsletter container bg-black">
+               <h2 class="text-primary">ÁREA RESTRITA</h2>
+                  <h3 class="text-danger">  Login </h3>
+                     <form class="form-inline" action="autenticado_usuario.php" method="POST" >
+                        <input class="form-control" type="email" name="email" placeholder="Login" value="<?php echo @$_SESSION['email']?>" ><br>
+                        <input class="form-control" type="password" name="senha" placeholder="Sua senha" value="<?php echo @$_SESSION['senha']?>" ><br>
+                        <button class="btn-success"> Entrar </button>
+                     </form>
+            </section>
+	      <br>
+	   <?php include('rodape.php');?>
 
-</body>
+   </body>
 </html>
