09b0c440dd639c219cbf7a5e40f95f225ceb6e9a
Nayara <nayara.lopes2007@gmail.com>
2020-10-13 22:52:09 -0300

    #Organizando URL

--- SPRINT_1/_sos_educa/index.php
+++ SPRINT_1/_sos_educa/index.php
@@ -19,7 +19,7 @@
         
         <div class="panel-body"><img style="width: 100%;" src="imagens/Português.png" class="img-responsive" class="rounded mx-auto d-block" style="width:75%" alt="Image"></div>
         <div class="panel-footer">Resumo de Português <p class="text-danger">Preço: <s>15,99 R$</s>12,99 R$</p></div>
-        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
       </div>
     </div>
     <div class="col-sm-4"> 
@@ -28,7 +28,7 @@
         <div class="panel-heading">Ofertas</div>
          <div class="panel-body"><img style="width: 100%;" src="imagens/Matemática (5).png" class="img-responsive" style="width:75%" alt="Image" ></div>
          <div class="panel-footer">Resumo de Matemática <p class="text-danger">Preço: <s>85,99 R$</s>79,99 R$</p></div>
-         <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+         <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
         </div>
     </div>
     <div class="col-sm-4"> 
@@ -36,7 +36,7 @@
         <div class="panel-heading">Ofertas</div>
         <div class="panel-body"><img style="width: 100%;" src="imagens/AOC (2).png" class="img-responsive" class="rounded mx-auto d-block" style="width:75%" alt="Image"></div>
         <div class="panel-footer">Resumo de AOC  <p class="text-danger">Preço: <s>45,99 R$</s>39,99 R$</p></div>
-        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
       </div>
     </div>
     <div class="col-sm-4">
@@ -46,7 +46,7 @@
         
         <div class="panel-body"><img style="width: 100%;" src="imagens/logicaProgramacao.jpeg" class="img-responsive" class="rounded mx-auto d-block" style="width:75%" alt="Image"></div>
         <div class="panel-footer">Resumo de Lógica de Programação <p class="text-danger">Preço: <s>18,99 R$</s>11,99 R$</p></div>
-        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
       </div>
 
     </div>
@@ -57,7 +57,7 @@
         
         <div class="panel-body"><img style="width: 100%;" src="imagens/Algoritimo.jpeg" class="img-responsive" class="rounded mx-auto d-block" style="width:75%" alt="Image"></div>
         <div class="panel-footer">Resumo de Algoritmo <p class="text-danger">Preço: <s>16,99 R$</s>10,99 R$</p></div>
-        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
       </div>
       
     </div>
@@ -68,7 +68,7 @@
         
         <div class="panel-body"><img style="width: 100%;" src="imagens/ingles.jpeg" class="img-responsive" class="rounded mx-auto d-block" style="width:75%" alt="Image"></div>
         <div class="panel-footer">Resumo de Inglês <p class="text-danger">Preço: <s>19,99 R$</s>15,99 R$</p></div>
-        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT%201/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
+        <a style="margin-left:30%; font-size:30px" href="http://localhost/Projeto_PI/SOS-EDUCA/SPRINT_1/_sos_educa/index_carrinho_cliente.php">Veja Mais</a>
       </div>
       
     </div>
