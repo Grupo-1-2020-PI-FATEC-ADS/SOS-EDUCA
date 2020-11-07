8cd92947dd70d41267783d553b177903bd08320f
Warlley Jhones Correia <60727051+warlley01@users.noreply.github.com>
2020-11-07 16:33:47 -0500

    Update navbar.php
    Adicionei: "position-absolute" na linha 9

--- SPRINT_1/_sos_educa/navbar.php
+++ SPRINT_1/_sos_educa/navbar.php
@@ -6,7 +6,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="index.php"><img  src="imagens/logo6.png" alt="placeholder+image" height="80px" width="80px"></a>
+      <a class="navbar-brand position-absolute" href="index.php"><img  src="imagens/logo6.png" alt="placeholder+image" height="80px" width="80px"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
