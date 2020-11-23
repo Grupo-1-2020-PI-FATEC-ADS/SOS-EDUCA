8ebc3c2af854cd3300878b6c0d6f7d71a848de00
Jeferson <jeferson.hms@hotmail.com>
2020-11-22 21:42:19 -0300

    Inserindo noticias

--- SPRINT_1/_sos_educa/blog_index.php
+++ SPRINT_1/_sos_educa/blog_index.php
@@ -5,7 +5,6 @@
 	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> <!-- https://fonts.google.com/ -->
     <link href="css/blog/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/blog/css/templatemo-xtra-blog.css" rel="stylesheet">
-
     <script src="css/blog/js/jquery.min.js"></script>
     <script src="css/blog/js/templatemo-script.js"></script>
 </head>
@@ -56,26 +55,24 @@
                         <div class="tm-post-link-inner">
                             <img src="https://veja.abril.com.br/wp-content/uploads/2020/08/STJ-1.jpeg?quality=70&strip=info&resize=680,453" alt="Image" class="img-fluid">                           
                         </div>
-                        <span class="position-absolute tm-new-badge">New</span>
-                        <h2 class="tm-pt-30 tm-color-primary tm-post-title"><a href="https://veja.abril.com.br/blog/radar-economico/brasil-sofre-seu-maior-ataque-hacker-da-historia/"target="_blank">Simple and useful HTML</a></h2>
+                        <span class="position-absolute tm-new-badge">Novo</span>
+                        <h2 class="tm-pt-30 tm-color-primary tm-post-title"><a href="https://veja.abril.com.br/blog/radar-economico/brasil-sofre-seu-maior-ataque-hacker-da-historia/"target="_blank">Brasil sofre seu maior ataque hacker da história </a></h2>
                     </a>                    
                     <p class="tm-pt-30">
-                        There is a clickable image with beautiful hover effect and active title link for each post item. 
-                        Left side is a sticky menu bar. Right side is a blog content that will scroll up and down.
+                    Superior Tribunal de Justiça, Ministério da Saúde e Distrito Federal foram alvos de cibercriminosos, que obtiveram acesso a informações sigilosas.
                     </p>
                 </article>
                 <article class="col-12 col-md-6 tm-post">
                     <hr class="tm-hr-primary">
                     <a href="blog_index.php" class="effect-lily tm-post-link tm-pt-60">
                         <div class=" tm-post-link-inner">
-                            <img src="css/blog/img/img-02.jpg" alt="Image" class="img-fluid">                            
+                            <img src="https://img.r7.com/images/whatsapp-23102020145150737?dimensions=460x305" alt="Image" class="img-fluid">                            
                         </div>
-                        <span class="position-absolute tm-new-badge">New</span>
-                        <h2 class="tm-pt-30 tm-color-primary tm-post-title">Multi-purpose blog template</h2>
-                    </a>                    
+                        <span class="position-absolute tm-new-badge">Novo</span>
+                        <h2 class="tm-pt-30 tm-color-primary tm-post-title"><a href="https://noticias.r7.com/tecnologia-e-ciencia/whatsapp-bloqueia-mais-de-1000-contas-por-disparos-em-massa-19112020"target="_blank"> WhatsApp bloqueia mais de 1.000 contas por disparos em massa</h2>
+                    </a>                   
                     <p class="tm-pt-30">
-                        <a rel="nofollow" href="https://templatemo.com/tm-553-xtra-blog" target="_blank">Xtra Blog</a>  is a multi-purpose HTML CSS template from TemplateMo website. 
-                        Blog list, single post, about, contact pages are included. Left sidebar fixed width and content area is a fluid full-width.
+                        <a rel="nofollow" href="https://templatemo.com/tm-553-xtra-blog" target="_blank"></a>  Denúncias foram recebidas em canal do TSE. Dentre as contas banidas, mais de 63% já tinham sido bloqueadas de forma proativa e automática 
                     </p>
                    
                 </article>
