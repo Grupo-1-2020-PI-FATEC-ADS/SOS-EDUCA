35a59fa0259acfd96725ca2b3b68d9b98f83aefe
Nayara <nayara.lopes2007@gmail.com>
2020-11-25 10:30:24 -0300

    Testando

--- SPRINT_1/_sos_educa/admin_editproduto.php
+++ SPRINT_1/_sos_educa/admin_editproduto.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Edit Product - Dashboard Admin Template</title>
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <!-- https://fonts.google.com/specimen/Roboto -->
     <link rel="stylesheet" href="css/admin/css/fontawesome.min.css">
