3c77954ad23ec4c77fc013f30dc3bb45d76290d1
Beatriz Nonato <bea_nonato@hotmail.com>
2020-11-27 07:38:21 -0300

    #cartao

--- SPRINT_1/_sos_educa/forma_pagamento.php
+++ SPRINT_1/_sos_educa/forma_pagamento.php
@@ -28,6 +28,13 @@
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans'>
     <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
     <link rel="stylesheet" href="cartao/css/style.css">
+    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/customized.css">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="cartao/js/index.js"></script>
   <title>SOS Educa</title>
 </head>
 <body>
@@ -48,96 +55,114 @@
               <form class="form" method="post" action="confirmar_produtos.php">
                 <input type="hidden" name="forma_pagamento" value="cartao">
                 <input type="hidden" name="idVenda" value="<?=$idVenda?>" />
-                <div class="row">
-                  <div class="col-md-8">
-                    <div class="form-group">
-                      
-                      <label for="numero_cartao">Número do Cartão</label>
-                      <input type="text" class="form-control" id="numero_cartao" name="numero_cartao"  maxlength="11">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="cod_seguranca">Código de Segurança</label>
-                      <input type="text" class="form-control" id="cod_seguranca" name="cod_seguranca"  maxlength="3">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-8">
-                  <label style="margin-right:100%">Parcelas</label>
-                  <fieldset class="fieldset-expiration">
-                      <div style="width: 166%;margin-top:-2%" class="select">
-                        <select id="parcelas" name="parcelas">
-                          <option></option>
-                          <option>1x s/juros</option>
-                          <option>2x s/juros</option>
-                          <option>3x s/juros</option>
-                          <option>4x s/juros</option>
-                          <option>5x s/juros</option>
-                          <option>6x s/juros</option>
-                          <option>7x s/juros</option>
-                          <option>8x s/juros</option>
-                          <option>9x s/juros</option>
-                          <option>10x s/juros</option>
-                        </select>
-                      </div>
-                    </fieldset>
-                  </div>
-                  <div class="col-md-4">
-                  <label style="margin-right:100%">Vencimento</label>
-                  <fieldset class="fieldset-expiration">    
-                      <div style="margin-top:-5%"class="select">
-                        <select id="card-expiration-month" name="data_validade">
-                          <option></option>
-                          <option>Jan</option>
-                          <option>Fev</option>
-                          <option>Mar</option>
-                          <option>Abr</option>
-                          <option>Mai</option>
-                          <option>Jun</option>
-                          <option>Jul</option>
-                          <option>Ago</option>
-                          <option>Set</option>
-                          <option>Out</option>
-                          <option>Nov</option>
-                          <option>Dez</option>
-                        </select>
-                      </div>
-                      <div style="margin-top:-5%" class="select">
-                        <select id="card-expiration-year">
-                          <option></option>
-                          <option>2020</option>
-                          <option>2021</option>
-                          <option>2022</option>
-                          <option>2023</option>
-                          <option>2024</option>
-                          <option>2025</option>
-                          <option>2026</option>
-                          <option>2027</option>
-                          <option>2028</option>
-                          <option>2029</option>
-                          <option>2030</option>
-                        </select>
-                      </div>
-                    </fieldset>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-8">
-                    <div class="form-group">
-                      <label for="nome_titular">Nome completo do titular</label>
-                      <input type="text" class="form-control" id="nome_titular" name="nome_titular">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="cupom_desconto">Cupom de Desconto</label>
-                      <input type="text" class="form-control" id="cupom_desconto" name="cupom_desconto">
-                    </div>
-                  </div>
-                </div>
-                <div class="checkbox">
+
+                <div class="checkout">
+  <div class="credit-card-box">
+    <div class="flip">
+      <div class="front">
+        <div class="chip"></div>
+        <div class="logo">
+          <img src="http://cdn.flaticon.com/svg/39/39134.svg" alt="" />
+        </div>
+        <div class="number"></div>
+        <div class="card-holder">
+          <label>Nome do Titular</label>
+          <div></div>
+        </div>
+        <div class="card-expiration-date">
+          <label>Data de Vencimento</label>
+          <div></div>
+        </div>
+      </div>
+      <div class="back">
+        <div class="strip"></div>
+        <div class="logo">
+          <img src="http://cdn.flaticon.com/svg/39/39134.svg" alt="" />
+        </div>
+        <div class="ccv">
+          <label>CVV</label>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <form class="form" autocomplete="off" novalidate method="post" action="confirmar_produtos.php">
+    <fieldset>
+      <label for="card-number">Número do Cartão</label>
+      <input type="num" id="card-number" name="numero_cartao" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-1" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-2" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-3" class="input-cart-number" maxlength="4" />
+      
+    </fieldset>
+    <fieldset>
+      <label for="card-holder">Nome</label>
+      <input type="text" id="card-holder" name="nome_titular"/>
+    </fieldset>
+    <fieldset class="fieldset-expiration">
+      <label for="card-expiration-month">Data de Vencimento</label>
+      <div class="select">
+        <select id="card-expiration-month" name="data_validade">
+          <option></option>
+          <option>Jan</option>
+          <option>Fev</option>
+          <option>Mar</option>
+          <option>Abr</option>
+          <option>Mai</option>
+          <option>Jun</option>
+          <option>Jul</option>
+          <option>Ago</option>
+          <option>Set</option>
+          <option>Out</option>
+          <option>Nov</option>
+          <option>Dez</option>
+        </select>
+      </div>
+      <div class="select">
+        <select id="card-expiration-year">
+          <option></option>
+          <option>2020</option>
+          <option>2021</option>
+          <option>2022</option>
+          <option>2023</option>
+          <option>2024</option>
+          <option>2025</option>
+          <option>2026</option>
+          <option>2027</option>
+          <option>2028</option>
+          <option>2029</option>
+          <option>2030</option>
+        </select>
+      </div>
+    </fieldset>
+    <fieldset class="fieldset-ccv">
+      <label for="card-ccv">CCV</label>
+      <input type="text" id="card-ccv" maxlength="3" name="cod_seguranca"/>
+    </fieldset>
+    <fieldset class="fieldset-expiration">
+      <label for="card-expiration-month">Parcelas</label>
+      <div class="select">
+        <select id="parcelas" name="parcelas">
+          <option></option>
+          <option>1x s/juros</option>
+          <option>2x s/juros</option>
+          <option>3x s/juros</option>
+          <option>4x s/juros</option>
+          <option>5x s/juros</option>
+          <option>6x s/juros</option>
+          <option>7x s/juros</option>
+          <option>8x s/juros</option>
+          <option>9x s/juros</option>
+          <option>10x s/juros</option>
+        </select>
+      </div>
+    </fieldset>
+    <fieldset>
+      <label for="card-holder">Cupom de Desconto</label>
+      <input type="text" id="card-holder" name="cupom_desconto"/>
+    </fieldset>
+
+    <div class="checkbox">
                   <label>
                     <input type="checkbox">
                     <strong>Lembrar deste cartão</strong>
@@ -145,7 +170,11 @@
                 </div>
                 <button type="submit" class="btn btn-primary btn-center">Continuar</button>
               </form>
+    
+
+               
             </div>
+          </div>
           </div>
           <div role="tabpanel" class="tab-pane" id="boleto">
             <div class="panel" style="padding-top:20px">
